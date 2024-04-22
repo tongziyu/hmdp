c84a26fd970d004c78645285f54ed83838bbaa05
tongziyu <8053980+tong-ziyu@user.noreply.gitee.com>
2024-04-22 18:30:36 +0800

    feat: 关注和取关

--- src/main/java/com/hmdp/controller/FollowController.java
+++ src/main/java/com/hmdp/controller/FollowController.java
@@ -1,9 +1,10 @@
 package com.hmdp.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import org.springframework.web.bind.annotation.RestController;
+import com.hmdp.dto.Result;
+import com.hmdp.service.IFollowService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * <p>
@@ -16,5 +17,23 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/follow")
 public class FollowController {
+    @Autowired
+    private IFollowService followService;
+
+    /**
+     * 关注用户
+     * @param id
+     * @return
+     */
+    @PutMapping("/{id}/{isFollow}")
+    public Result followById(@PathVariable Long id,@PathVariable Boolean isFollow){
+
+        return followService.followById(id,isFollow);
+    }
+
+    @GetMapping("/or/not/{id}")
+    public Result followOrNot(@PathVariable Long id){
+        return followService.followOrNotById(id);
+    }
 
 }
--- src/main/java/com/hmdp/service/impl/FollowServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/FollowServiceImpl.java
@@ -1,20 +1,76 @@
 package com.hmdp.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.hmdp.dto.Result;
+import com.hmdp.dto.UserDTO;
 import com.hmdp.entity.Follow;
 import com.hmdp.mapper.FollowMapper;
 import com.hmdp.service.IFollowService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.utils.UserHolder;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * <p>
  *  服务实现类
  * </p>
  *
- * @author 虎哥
- * @since 2021-12-22
  */
 @Service
 public class FollowServiceImpl extends ServiceImpl<FollowMapper, Follow> implements IFollowService {
+    @Autowired
+    private FollowMapper followMapper;
 
+    /**
+     * 关注用户
+     * @param id
+     * @return
+     */
+    @Override
+    public Result followById(Long id,Boolean isFollow) {
+        Long myUserId = UserHolder.getUser().getId();
+        if (isFollow){
+            Follow follow = new Follow();
+            follow.setUserId(myUserId);
+            follow.setFollowUserId(id);
+            followMapper.insert(follow);
+            return Result.ok();
+        }else{
+            Map<String,Object> hashMap = new HashMap();
+            hashMap.put("user_id",UserHolder.getUser().getId().toString());
+            hashMap.put("follow_user_id",id.toString());
+            followMapper.deleteByMap(hashMap);
+            return Result.ok();
+        }
+    }
+
+
+    /**
+     * 查看是否关注了
+     * @param id
+     * @return
+     */
+    @Override
+    public Result followOrNotById(Long id) {
+        UserDTO user = UserHolder.getUser();
+        if (user == null){
+            return Result.ok();
+        }
+
+        Long myUserId = UserHolder.getUser().getId();
+        QueryWrapper queryWrapper = new QueryWrapper();
+        queryWrapper.eq("user_id",myUserId);
+        queryWrapper.eq("follow_user_id",id);
+
+        Follow follow = followMapper.selectOne(queryWrapper);
+        if (follow == null){
+            return Result.ok(false);
+        }
+
+        return Result.ok(true);
+    }
 }
